b96aaa685aa5d98584ae716e1574185fda4a4838
Sera91 <serafina.digioia@inaf.it>
2023-04-18 22:50:35 +0200

    modified:   setting.sh

--- setting.sh
+++ setting.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 git clone https://github.com/Sera91/Dask-tutorial-SA.git
 
 module load chpc/cuda/11.2/SXM2/11.2 chpc/openmpi/4.0.0/gcc-7.3.0 chpc/openblas/0.2.19/gcc-6.1.0 chpc/astro/anaconda/3
