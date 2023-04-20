52f0ae1981731aad736779295d9cc6c8c703f16e
Serafina Di Gioia <eirene1991@gmail.com>
2023-04-20 14:19:47 +0200

    Update compile.sh

--- Ivan-lecture-material/3D-template-codes/compile.sh
+++ Ivan-lecture-material/3D-template-codes/compile.sh
@@ -3,4 +3,4 @@
 C_FLAGS="-O3"
 # -xMIC-AVX512"
 mpicc ${C_FLAGS} -c benchmark_LBE3D_IO.c -I/home/apps/chpc/compmech/spack/opt/spack/linux-centos7-haswell/gcc-9.2.0/hdf5-1.12.2-gvydabuqenshywafz46o2cqkgoa22xvo/include
-mpicc ${C_FLAGS} -o benchmark_LBE3D_IO.x benchmark_LBE3D_IO.o -L/home/apps/chpc/compmech/spack/opt/spack/linux-centos7-haswell/gcc-9.2.0/hdf5-1.12.2-gvydabuqenshywafz46o2cqkgoa22xvo/lib -lhdf5
+mpicc ${C_FLAGS} -o benchmark_LBE3D_IO.x benchmark_LBE3D_IO.o -L/home/apps/chpc/compmech/spack/opt/spack/linux-centos7-haswell/gcc-9.2.0/hdf5-1.12.2-gvydabuqenshywafz46o2cqkgoa22xvo/lib -lhdf5 -lm
