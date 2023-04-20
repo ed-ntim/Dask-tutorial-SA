c1f48f5c55506e5d41f0c31fef15e4789937b7ad
Sera91 <serafina.digioia@inaf.it>
2023-04-20 10:53:35 +0200

    new file:   ML-lecture-material/README.md 	new file:   ML-lecture-material/setting.sh

---
+++ ML-lecture-material/setting.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+module load chpc/cuda/11.2/SXM2/11.2 chpc/openmpi/4.0.0/gcc-7.3.0 chpc/openblas/0.2.19/gcc-6.1.0 chpc/astro/anaconda/3
+
+cd Dask-tutorial-SA
+
+conda env create -f Sera_env.yml
+
+
+cat conda_init.txt >>${HOME}/.bashrc
+
+source ${HOME}/.bashrc
+
+conda activate py310
+
+
+
